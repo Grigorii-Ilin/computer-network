feb705d5b663395604dc57ea0dbf5155b090a925
Grigorii-Ilin <grigory1728@gmail.com>
2020-03-16 09:55:52 +0300

    client - server done

--- socket/client/client.c
+++ socket/client/client.c
@@ -1,26 +1,30 @@
 #include <windows.h> 
 #include <stdio.h> 
+
 #define PORT 3550 
-#define MAXDATASIZE 100 
-int main(int argc, char* argv[])
-{
+#define MAXDATASIZE 250 
+
+int main(int argc, char* argv[]){
 	WSADATA wsdata;
-	WSAStartup(0x0101, &wsdata);
-	int fd, numbytes;
+	//WSAStartup(0x0101,&wsdata); 
+	//https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-wsastartup
+	WSAStartup(MAKEWORD(2, 2), &wsdata);
+	int iConnectedSocket;
+	int numbytes;
 	char buf[MAXDATASIZE];
 	struct hostent* he;
 	struct sockaddr_in server;
-	if (argc != 2)
-	{
-		printf("Usage: %s <IP Address>\n", argv[0]);
-		exit(-1);
-	}
-	if ((he = gethostbyname(argv[1])) == NULL)
+	//if (argc != 2)
+	//{
+	//	printf("Usage: %s <IP Address>\n", argv[0]);
+	//	exit(-1);
+	//}
+	if ((he = gethostbyname("localhost")) == NULL)	//(argv[1])
 	{
 		printf("gethostbyname() error\n");
 		exit(-1);
 	}
-	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	if ((iConnectedSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
 		printf("socket() error\n");
 		exit(-1);
@@ -28,19 +32,25 @@ int main(int argc, char* argv[])
 	server.sin_family = AF_INET;
 	server.sin_port = htons(PORT);
 	server.sin_addr = *((struct in_addr*)he->h_addr);
-	if (connect(fd, (struct sockaddr*) & server, sizeof(struct sockaddr)) == -1)
+	if (connect(iConnectedSocket, (struct sockaddr*)&server, sizeof(struct sockaddr)) == -1)
 	{
 		printf("connect() error\n");
 		exit(-1);
 	}
-	if ((numbytes = recv(fd, buf, MAXDATASIZE, 0)) == -1)
+	if ((numbytes = recv(iConnectedSocket, buf, MAXDATASIZE, 0)) == -1) // like read in linux
 	{
 		printf("recv() error\n");
 		exit(-1);
 	}
 	buf[numbytes] = '\0';
 	printf("Server Message: %s\n", buf);
-	closesocket(fd);
+
+
+	char* sendbuf = "22222";
+	send(iConnectedSocket, sendbuf, (int)strlen(sendbuf), 0);
+
+	closesocket(iConnectedSocket);
 	WSACleanup();
-	return -1;
+
+	return 0;
 }
--- socket/server/server.c
+++ socket/server/server.c
@@ -1,11 +1,15 @@
 #include <stdio.h> 
 #include <windows.h>
+
 #define PORT 3550
-#define BACKLOG 2
+//#define BACKLOG 2
+#define MAXDATASIZE 250 
 
 int main(int argc, char** argv) {
     WSADATA wsdata; 
-    WSAStartup(0x0101,&wsdata); 
+    //WSAStartup(0x0101,&wsdata); 
+    //https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-wsastartup
+    WSAStartup(MAKEWORD(2, 2), &wsdata);
     int fd, fd2;
     struct sockaddr_in server;
     struct sockaddr_in client; 
@@ -25,23 +29,39 @@ int main(int argc, char** argv) {
         exit (-1); 
     } 
 
-    if (listen (fd,BACKLOG) == -1) { 
+    if (listen (fd,2) == -1) { 
         printf ("listen() error\n"); 
         exit (-1); 
     } 
 
-    while (1){ 
+    boolean clientConnected = FALSE;
+    do{
         sin_size=sizeof (struct sockaddr_in); 
-        if ((fd2 = accept(fd,(struct sockaddr *)&client,&sin_size)) == -1){ 
+        if ((fd2 = accept(fd,(struct sockaddr*)&client,&sin_size)) == -1){ 
             printf ("accept() error\n"); 
             exit (-1); 
         } 
         printf ("You got a connection from %s\n",inet_ntoa(client.sin_addr) ); 
-        send (fd2,"Welcome to my server.\n",22,0); 
-        closesocket (fd2); 
-        WSACleanup(); 
-        return -1; 
+        send (fd2,"111111\n", (int)strlen("111111\n"),0);
+
+        clientConnected = TRUE;
+
+    } while (clientConnected==FALSE);
+
+
+    int numbytes;
+    char buf[MAXDATASIZE];
+    if ((numbytes = recv(fd2, buf, MAXDATASIZE, 0)) == -1) // like read in linux
+    {
+        printf("recv() error\n");
+        exit(-1);
     }
+    buf[numbytes] = '\0';
+    printf("Client Message: %s\n", buf);
+
+
+    closesocket (fd2); 
+    WSACleanup();
 
     return 0;
 }
