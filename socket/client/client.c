0679cd1666759f07ba8c017581985cd6fd81a049
Grigorii-Ilin <grigory1728@gmail.com>
2020-03-16 14:04:57 +0300

    socket done

--- socket/client/client.c
+++ socket/client/client.c
@@ -1,53 +1,40 @@
 #include <windows.h> 
 #include <stdio.h> 
+#include <locale.h>
+#include "vigenere_lib.h"
 
 #define PORT 3550 
 #define MAXDATASIZE 250 
 
 int main(int argc, char* argv[]){
+	setlocale(LC_ALL, "RUS");
+
+	printf("%s", "¬ведите пароль (A-Z):\n");
+	char key[255];
+	scanf_s("%254s", key, 255);
+
 	WSADATA wsdata;
-	//WSAStartup(0x0101,&wsdata); 
 	//https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-wsastartup
 	WSAStartup(MAKEWORD(2, 2), &wsdata);
-	int iConnectedSocket;
-	int numbytes;
-	char buf[MAXDATASIZE];
-	struct hostent* he;
+
+	struct hostent* he = gethostbyname("localhost");
+	int iConnectedSocket = socket(AF_INET, SOCK_STREAM, 0);
+
 	struct sockaddr_in server;
-	//if (argc != 2)
-	//{
-	//	printf("Usage: %s <IP Address>\n", argv[0]);
-	//	exit(-1);
-	//}
-	if ((he = gethostbyname("localhost")) == NULL)	//(argv[1])
-	{
-		printf("gethostbyname() error\n");
-		exit(-1);
-	}
-	if ((iConnectedSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	{
-		printf("socket() error\n");
-		exit(-1);
-	}
 	server.sin_family = AF_INET;
 	server.sin_port = htons(PORT);
 	server.sin_addr = *((struct in_addr*)he->h_addr);
-	if (connect(iConnectedSocket, (struct sockaddr*)&server, sizeof(struct sockaddr)) == -1)
-	{
-		printf("connect() error\n");
-		exit(-1);
-	}
-	if ((numbytes = recv(iConnectedSocket, buf, MAXDATASIZE, 0)) == -1) // like read in linux
-	{
-		printf("recv() error\n");
-		exit(-1);
-	}
+
+	connect(iConnectedSocket, (struct sockaddr*) & server, sizeof(struct sockaddr));
+
+	char buf[MAXDATASIZE];
+	int numbytes = recv(iConnectedSocket, buf, MAXDATASIZE, 0);
 	buf[numbytes] = '\0';
-	printf("Server Message: %s\n", buf);
+	printf("—ообщение от сервера: %s\n", buf);
 
+	decrypt(key, strlen(key), buf, numbytes);
 
-	char* sendbuf = "22222";
-	send(iConnectedSocket, sendbuf, (int)strlen(sendbuf), 0);
+	send(iConnectedSocket, buf, (int)strlen(buf), 0);
 
 	closesocket(iConnectedSocket);
 	WSACleanup();
--- socket/server/server.c
+++ socket/server/server.c
@@ -1,66 +1,56 @@
 #include <stdio.h> 
 #include <windows.h>
+#include <locale.h>
+#include "vigenere_lib.h"
+
 
 #define PORT 3550
-//#define BACKLOG 2
 #define MAXDATASIZE 250 
 
-int main(int argc, char** argv) {
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    printf("%s", "Введите пароль (A-Z):\n");
+    char key[255];
+    scanf_s("%254s", key, 255);
+
+    printf("%s", "Введите текст для шифрования (A-Z):\n");
+    char txt[255];
+    scanf_s("%254s", txt, 255);
+
+    encrypt(key, strlen(key), txt, strlen(txt));
+
     WSADATA wsdata; 
-    //WSAStartup(0x0101,&wsdata); 
     //https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-wsastartup
     WSAStartup(MAKEWORD(2, 2), &wsdata);
-    int fd, fd2;
+
+    int iSocket1;
+    iSocket1 = socket(AF_INET, SOCK_STREAM, 0);
+
     struct sockaddr_in server;
-    struct sockaddr_in client; 
-    int sin_size; 
-
-    if ((fd=socket(AF_INET, SOCK_STREAM, 0)) == -1 ) { 
-        printf("socket() error\n"); 
-        exit(-1); 
-    } 
-
     server.sin_family = AF_INET; 
     server.sin_port = htons(PORT);
     server.sin_addr.s_addr = INADDR_ANY; 
 
-    if (bind(fd,(struct sockaddr*)&server, sizeof(struct sockaddr))==-1) { 
-        printf ("bind() error\n"); 
-        exit (-1); 
-    } 
+    bind(iSocket1, (struct sockaddr*) & server, sizeof(struct sockaddr));
 
-    if (listen (fd,2) == -1) { 
-        printf ("listen() error\n"); 
-        exit (-1); 
-    } 
+    printf("%s", "Ожидаем клиента...\n");
+    listen(iSocket1, 1);
 
-    boolean clientConnected = FALSE;
-    do{
-        sin_size=sizeof (struct sockaddr_in); 
-        if ((fd2 = accept(fd,(struct sockaddr*)&client,&sin_size)) == -1){ 
-            printf ("accept() error\n"); 
-            exit (-1); 
-        } 
-        printf ("You got a connection from %s\n",inet_ntoa(client.sin_addr) ); 
-        send (fd2,"111111\n", (int)strlen("111111\n"),0);
-
-        clientConnected = TRUE;
-
-    } while (clientConnected==FALSE);
-
-
+    int iSocket2;
+    struct sockaddr_in client;
+    int sin_size;
+    sin_size = sizeof(struct sockaddr_in);
+    iSocket2 = accept(iSocket1, (struct sockaddr*) & client, &sin_size);
+    send(iSocket2, txt, (int)strlen(txt), 0);
+ 
     int numbytes;
     char buf[MAXDATASIZE];
-    if ((numbytes = recv(fd2, buf, MAXDATASIZE, 0)) == -1) // like read in linux
-    {
-        printf("recv() error\n");
-        exit(-1);
-    }
+    numbytes = recv(iSocket2, buf, MAXDATASIZE, 0);// like read in linux
     buf[numbytes] = '\0';
-    printf("Client Message: %s\n", buf);
+    printf("Сообщение от клиента: %s\n", buf);
 
-
-    closesocket (fd2); 
+    closesocket(iSocket2);
     WSACleanup();
 
     return 0;
